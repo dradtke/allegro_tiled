aa522d701794648a746eef6923ffcabe9c9a509e
Damien Radtke <admin@damienradtke.org>
2012-06-21 13:24:55 -0500

    Updating source license comments.

--- src/internal.h
+++ src/internal.h
@@ -1,3 +1,20 @@
+/*
+ * This addon adds Tiled map support to the Allegro game library.
+ * Copyright (c) 2012 Damien Radtke - www.damienradtke.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For more information, visit http://www.gnu.org/copyleft
+ */
+
 #ifndef INTERNAL_H
 #define INTERNAL_H
 
