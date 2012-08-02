a967e1dca40fb862a4bce4fb027a86235c52e507
Damien Radtke <admin@damienradtke.org>
2012-08-01 20:55:20 -0500

    Updated data types.

--- src/parser.c
+++ src/parser.c
@@ -39,13 +39,13 @@ static inline _AL_LIST *create_list(size_t capacity)
  */
 static void decode_layer_data(xmlNode *data_node, ALLEGRO_MAP_LAYER *layer)
 {
-	gchar *str = trim((gchar *)data_node->children->content);
-	gint datalen = layer->width * layer->height;
-	layer->data = (gchar *)calloc(datalen, sizeof(gchar));
+	char *str = trim((char *)data_node->children->content);
+	int datalen = layer->width * layer->height;
+	layer->data = (char *)calloc(datalen, sizeof(char));
 
 	char *encoding = get_xml_attribute(data_node, "encoding");
 	if (!encoding) {
-		gint i = 0;
+		int i = 0;
 		GSList *tiles = get_children_for_name(data_node, "tile");
 		GSList *tile_item = tiles;
 		while (tile_item) {
@@ -61,7 +61,7 @@ static void decode_layer_data(xmlNode *data_node, ALLEGRO_MAP_LAYER *layer)
 	else if (!strcmp(encoding, "base64")) {
 		// decompress
 		gsize rawlen;
-		guchar *rawdata = g_base64_decode(str, &rawlen);
+		unsigned char *rawdata = g_base64_decode(str, &rawlen);
 
 		// check the compression
 		char *compression = get_xml_attribute(data_node, "compression");
@@ -80,26 +80,26 @@ static void decode_layer_data(xmlNode *data_node, ALLEGRO_MAP_LAYER *layer)
 			ALLEGRO_FILE *datadest = al_make_temp_file("XXXXXX", NULL);
 
 			// decompress and print an error if it failed
-			gint status = inf(datasrc, datadest);
+			int status = inf(datasrc, datadest);
 			if (status)
 				zerr(status);
 
 			// flush data and get the file length
 			al_fflush(datadest);
-			gint len = al_fsize(datadest);
+			int len = al_fsize(datadest);
 
 			// read in the file
 			al_fseek(datadest, 0, ALLEGRO_SEEK_SET);
-			gchar *data = (gchar *)calloc(len, sizeof(char));
+			char *data = (char *)calloc(len, sizeof(char));
 			if (al_fread(datadest, data, len) != len) {
 				fprintf(stderr, "Error: failed to read in map data\n");
 				return;
 			}
 
 			// every tile id takes 4 bytes
-			gint i;
+			int i;
 			for (i = 0; i<len; i += 4) {
-				gint tileid = 0;
+				int tileid = 0;
 				tileid |= data[i];
 				tileid |= data[i+1] << 8;
 				tileid |= data[i+2] << 16;
@@ -117,9 +117,9 @@ static void decode_layer_data(xmlNode *data_node, ALLEGRO_MAP_LAYER *layer)
 		}
 		else {
 			// TODO: verify that this still works
-			gint i;
+			int i;
 			for (i = 0; i<rawlen; i += 4) {
-				gint tileid = 0;
+				int tileid = 0;
 				tileid |= rawdata[i];
 				tileid |= rawdata[i+1] << 8;
 				tileid |= rawdata[i+2] << 16;
@@ -132,7 +132,7 @@ static void decode_layer_data(xmlNode *data_node, ALLEGRO_MAP_LAYER *layer)
 		g_free(rawdata);
 	}
 	else if (!strcmp(encoding, "csv")) {
-		gint i;
+		int i;
 		for (i = 0; i<datalen; i++) {
 			char *id = strtok((i == 0 ? str : NULL), ",");
 			layer->data[i] = atoi(id);
@@ -184,10 +184,10 @@ static GHashTable *parse_properties(xmlNode *node)
 		xmlNode *property_node = (xmlNode*)property_item->data;
 		property_item = g_slist_next(property_item);
 
-		gchar *name = g_strdup(get_xml_attribute(property_node, "name"));
-		gchar *value = get_xml_attribute(property_node, "value");
+		char *name = g_strdup(get_xml_attribute(property_node, "name"));
+		char *value = get_xml_attribute(property_node, "value");
 		if (!value) {
-			value = (gchar*)xmlNodeGetContent(property_node);
+			value = (char*)xmlNodeGetContent(property_node);
 		}
 
 		value = g_strdup(value);
@@ -369,10 +369,10 @@ ALLEGRO_MAP *al_open_map(const char *dir, const char *filename)
 				// create this tile's bitmap if it hasn't been yet
 				if (!tile->bitmap) {
 					ALLEGRO_MAP_TILESET *tileset = tile->tileset;
-					gint id = tile->id - tileset->firstgid;
-					gint width = tileset->width / tileset->tilewidth;
-					gint x = (id % width) * tileset->tilewidth;
-					gint y = (id / width) * tileset->tileheight;
+					int id = tile->id - tileset->firstgid;
+					int width = tileset->width / tileset->tilewidth;
+					int x = (id % width) * tileset->tilewidth;
+					int y = (id / width) * tileset->tileheight;
 					tile->bitmap = al_create_sub_bitmap(
 							tileset->bitmap,
 							x, y,
